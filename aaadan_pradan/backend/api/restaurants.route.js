6a304ef5f1cf233a12b5bb0b43ddba0abd141e0e
Exterminator11 <rachit1031@gmail.com>
2023-02-12 06:51:40 +0530

    removed backend file

--- aaadan_pradan/backend/api/restaurants.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import RestaurantsControl from './restaurants.controller.js'
-
-const router=express.Router()
-
-router.route("/").get(RestaurantsControl.apiGetRestaurants)
-
-export default router
